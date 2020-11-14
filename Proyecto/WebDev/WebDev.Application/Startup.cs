301d8d7282320a34e7caa571b67f5e3bcf496340
Julio Robles <jucer74@hotmail.com>
2020-11-14 11:33:16 -0500

    Connect Web-API

--- Proyecto/WebDev/WebDev.Application/Startup.cs
+++ Proyecto/WebDev/WebDev.Application/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebDev.Application.Config;
 
 namespace WebDev.Application
 {
@@ -19,6 +20,7 @@ namespace WebDev.Application
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllersWithViews();
+      services.Configure<ApiConfiguration>(Configuration.GetSection("ApiConfiguration"));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
