eacc24a7be43ff33f70c70d098648334027e3ae4
Julio Robles <jucer74@hotmail.com>
2020-11-23 04:57:10 -0500

    Sesion-07

--- Proyecto/KotlinAPIUsers/kotlin-users-service-api/src/main/kotlin/com/puj/admincenter/controller/UserController.kt
+++ Proyecto/KotlinAPIUsers/kotlin-users-service-api/src/main/kotlin/com/puj/admincenter/controller/UserController.kt
@@ -11,11 +11,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.web.bind.annotation.*
-// import org.springframework.web.bind.annotation.GetMapping
-// import org.springframework.web.bind.annotation.PostMapping
-// import org.springframework.web.bind.annotation.PutMapping
-// import org.springframework.web.bind.annotation.DeleteMapping
-// import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
 import javax.validation.Valid
