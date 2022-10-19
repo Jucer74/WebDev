cd3bb0758fa880d5bbdc95c7119d2d710911ab29
Julio Robles <Jucer74@hotmail.com>
2022-10-19 02:14:29 -0500

    Update Service

--- Sesiones/Session-10-Authorization/EmployeesWeb/EmployeesWeb.Application/Controllers/HomeController.cs
+++ Sesiones/Session-10-Authorization/EmployeesWeb/EmployeesWeb.Application/Controllers/HomeController.cs
@@ -6,11 +6,10 @@ namespace EmployeesWeb.Application.Controllers
 {
    public class HomeController : Controller
    {
-      private readonly ILogger<HomeController> _logger;
 
-      public HomeController(ILogger<HomeController> logger)
+      public HomeController(IConfiguration configuration)
       {
-         _logger = logger;
+         
       }
 
       public IActionResult Index()
@@ -27,6 +26,57 @@ namespace EmployeesWeb.Application.Controllers
       public IActionResult Error()
       {
          return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+
+      [HttpGet]
+      [Route("[controller]/[action]")]
+      public IActionResult Login()
+      {
+         return View();
+      }
+
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public async Task<IActionResult> Login(Login login)
+      {
+         try
+         {
+            if (ModelState.IsValid)
+            {
+               // Llamar a la API para validar el Login
+               if (await IsValidUser(login))
+               {
+                  return Redirect(nameof(Index));
+               }
+               ModelState.AddModelError(string.Empty, "Intento de inicio de sesión no válido.");
+            }
+         }
+         catch (Exception ex)
+         {
+            ModelState.AddModelError(string.Empty, ex.Message);
+         }
+         return View();
+      }
+
+      public IActionResult Logout()
+      {
+         HttpContext.Session.SetString("IsUserLogged", "false");
+         return RedirectToAction(nameof(Index));
+      }
+
+      private async Task<bool> IsValidUser(Login login)
+      {
+         var tokenDto = await loginService.ValidUser(LoginMapper.ToDto(login));
+         if (tokenDto == null)
+         {
+            HttpContext.Session.SetString("IsUserLogged", "false");
+            return false;
+         }
+         HttpContext.Session.SetString("IsUserLogged", "true");
+         HttpContext.Session.SetString("User", tokenDto.Name);
+         HttpContext.Session.SetString("TokenData", JsonConvert.SerializeObject(TokenMapper.ToEntity(tokenDto)));
+
+         return true;
       }
    }
 }
