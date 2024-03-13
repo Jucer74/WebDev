77de0d3b5f0e112ee78f906287bb28f8e9136772
Julio Robles <Jucer74@Hotmail.com>
2024-03-12 22:17:20 -0500

    Bad Practices

--- Proyecto/Api/WebDev.Api/WebDev.Api/Controllers/UsersController.cs
+++ Proyecto/Api/WebDev.Api/WebDev.Api/Controllers/UsersController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,107 +8,98 @@ using WebDev.Api.Model;
 
 namespace WebDev.Api.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class UsersController : ControllerBase
-  {
-    private readonly AppDbContext _context;
-
-    public UsersController(AppDbContext context)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
     {
-      _context = context;
-    }
+        private readonly AppDbContext _context;
 
-    // GET: api/Users
-    [HttpGet]
-    [Authorize]
-    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-    {
-      return await _context.Users.ToListAsync();
-    }
-
-    // GET: api/Users/5
-    [HttpGet("{id}")]
-    [Authorize]
-    public async Task<ActionResult<User>> GetUser(int id)
-    {
-      var user = await _context.Users.FindAsync(id);
-
-      if (user == null)
-      {
-        return NotFound();
-      }
-
-      return user;
-    }
-
-    // POST: api/Users
-    // To protect from overposting attacks, enable the specific properties you want to bind to, for
-    // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-    [HttpPost]
-    [Authorize] 
-    public async Task<ActionResult<UserIdResponse>> PostUser(User user)
-    {
-      _context.Users.Add(user);
-      await _context.SaveChangesAsync();
-
-      return Created(string.Empty, new UserIdResponse { Id = user.Id });
-    }
-
-    // PUT: api/Users/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to, for
-    // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-    [HttpPut("{id}")]
-    [Authorize]
-    public async Task<IActionResult> PutUser(int id, User user)
-    {
-      if (id != user.Id)
-      {
-        return BadRequest();
-      }
-
-      _context.Entry(user).State = EntityState.Modified;
-
-      try
-      {
-        await _context.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!UserExists(id))
+        public UsersController()
         {
-          return NotFound();
+            _context = GetDbContext("UsersDb");
         }
-        else
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-          throw;
+            return await _context.Users.ToListAsync();
         }
-      }
 
-      return NoContent();
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<UserIdResponse>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Created(string.Empty, new UserIdResponse { Id = user.Id });
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!UserExists(id))
+            {
+                return NotFound();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
+        public static AppDbContext GetDbContext(string databaseName)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer($@"Server=localhost;Database={databaseName};User ID=Admin;Password=P4ssw0rd*01;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+            return new AppDbContext(optionsBuilder.Options);
+        }
     }
-
-
-    // DELETE: api/Users/5
-    [HttpDelete("{id}")]
-    [Authorize] 
-    public async Task<ActionResult<User>> DeleteUser(int id)
-    {
-      var user = await _context.Users.FindAsync(id);
-      if (user == null)
-      {
-        return NotFound();
-      }
-
-      _context.Users.Remove(user);
-      await _context.SaveChangesAsync();
-
-      return NoContent();
-    }
-
-    private bool UserExists(int id)
-    {
-      return _context.Users.Any(e => e.Id == id);
-    }
-  }
 }
--- Proyecto/Api/WebDev.Api/WebDev.Api/Startup.cs
+++ Proyecto/Api/WebDev.Api/WebDev.Api/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,111 +8,110 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Text;
-using WebDev.Api.Context;
 
 namespace WebDev.Api
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddAuthentication(x => 
+        public Startup(IConfiguration configuration)
         {
-          x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-          x.DefaultChallengeScheme= JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddJwtBearer(options =>
-        {
-          options.RequireHttpsMetadata = true;
-          options.SaveToken = true;
-          options.TokenValidationParameters = new TokenValidationParameters()
-          {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = Configuration["JWT:Issuer"],
-            ValidAudience = Configuration["JWT:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:SecretKey"]))
-          };
-        });
+            Configuration = configuration;
+        }
 
-      services.AddCors();
-      services.AddControllers();
-      services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CnnStr")));
-      services.AddSwaggerGen(s => 
-      {
-        s.SwaggerDoc("v1", new OpenApiInfo { Title = "WebDev API", Version = "v1" });
-        s.AddSecurityDefinition("Bearer",
-          new OpenApiSecurityScheme
-          {
-            Scheme= "Bearer",
-            In = ParameterLocation.Header,
-            Description = "Please enter into field the word 'Bearer' following by space and JWT",
-            Name = "Authorization",
-            Type =  SecuritySchemeType.ApiKey
-          });
-        s.AddSecurityRequirement(new OpenApiSecurityRequirement
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-          {
+            services.AddAuthentication(x =>
+              {
+                  x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                  x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+              })
+              .AddJwtBearer(options =>
+              {
+                  options.RequireHttpsMetadata = true;
+                  options.SaveToken = true;
+                  options.TokenValidationParameters = new TokenValidationParameters()
+                  {
+                      ValidateIssuer = false,
+                      ValidateAudience = false,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+                      ValidIssuer = Configuration["JWT:Issuer"],
+                      ValidAudience = Configuration["JWT:Audience"],
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:SecretKey"]))
+                  };
+              });
+
+            services.AddCors();
+            services.AddControllers();
+            //services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CnnStr")));
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = "WebDev API", Version = "v1" });
+                s.AddSecurityDefinition("Bearer",
             new OpenApiSecurityScheme
             {
-              Reference = new OpenApiReference
+                Scheme = "Bearer",
+                In = ParameterLocation.Header,
+                Description = "Please enter into field the word 'Bearer' following by space and JWT",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement
               {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
+            {
+              new OpenApiSecurityScheme
+              {
+                Reference = new OpenApiReference
+                {
+                  Type = ReferenceType.SecurityScheme,
+                  Id = "Bearer"
+                }
+              },
+              Array.Empty<string>()
               }
-            },
-            Array.Empty<string>()
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:3000");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
             }
-          });
-        });
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML. JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint
+            app.UseSwaggerUI(s => s.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API"));
+
+            // Add the Authentication to Enable the JWT Service
+            //app.UseAuthentication();
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      app.UseCors(options =>
-      {
-        options.WithOrigins("http://localhost:3000");
-        options.AllowAnyMethod();
-        options.AllowAnyHeader();
-      });
-
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseHttpsRedirection();
-      app.UseAuthentication();
-
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-
-      // Enable middleware to serve generated Swagger as a JSON endpoint.
-      app.UseSwagger();
-
-      // Enable middleware to serve swagger-ui (HTML. JS, CSS, etc.),
-      // specifying the Swagger JSON endpoint
-      app.UseSwaggerUI(s => s.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API"));
-
-      // Add the Authentication to Enable the JWT Service
-      //app.UseAuthentication();
-    }
-  }
 }
